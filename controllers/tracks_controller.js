b0cd34311f54b4e6b5b2d0cb5c4a360cc29233e7
Andrea Pantoja <apantojab@MacBook-Pro-de-Andrea.local>
2016-01-21 19:14:13 +0100

    .

--- controllers/tracks_controller.js
+++ controllers/tracks_controller.js
@@ -18,7 +18,6 @@ exports.findTrackByName = function(req, res){
 
 //POST - Inserta una nueva cancion en la DB
 exports.addTrack = function(req, res){
-    //PRODUCCION: var urlNAS = "../mnt/nas/";
 	var urlNAS = "../mnt/nas/";
 
 	if (req.method == 'POST') {
@@ -31,25 +30,18 @@ exports.addTrack = function(req, res){
         var contador = 0;
         req.on('data', function (data) {
             body += data;
-            //el primer dato que se envia es el nombre del fichero y un trozo de cancion
-            //el resto de datos que llegan, es solo cancion.
+
             if (contador == 0){
             	var stringData = data.toString();
 
-            	//EJ: prueba.mp3 -------XXXXXXX
             	stringData = stringData.substr(stringData.indexOf('filename')+13);
                 
-            	//cortamos despues del . y 3 caracteres
             	stringData = stringData.substr(0,stringData.indexOf('.')+4);
-                
-            	//guardamos el nombre de fichero finalmente.
+    
             	filename = stringData;
-            	//guardamos el nombre del fichero a guardar.
-            	//EJ: /mnt/nas/1251241213_Prueba.mp3
-            	//metemos un numero random, para asegurar que no hay ninguno igual
+
             	var random = Math.floor((Math.random() * 100) + 1);
 
-                //para prevenir posibles errores de que no encuentre el nombre.
                 console.log("FILENAME: "+filename);
                 if (fileName == ""){
                     filename = ".mp3";
@@ -58,9 +50,7 @@ exports.addTrack = function(req, res){
             	tempName = new Date().getTime()+random+'_'+filename;
             	mp3File = urlNAS + tempName;
 
-            	//init el writeStream
             	mp3_file = fs.createWriteStream(mp3File);
-            	//escribimos el trozo de cancion que llega en este primer data.
             	mp3_file.write(data);
             	contador++;
             }else{
@@ -72,7 +62,6 @@ exports.addTrack = function(req, res){
 
             mp3_file.end();
             res.writeHead(200, {'Content-Type': 'text/html'});
-            //devolvemos el nombre de la cancion guardada:
         	res.end(tempName);
         });
 
@@ -82,7 +71,6 @@ exports.addTrack = function(req, res){
 
 //DELETE - Borramos una cancion por su nombre
 exports.deleteTrackByName = function(req,res){
-    //PRODUCCION: var urlNAS = "../mnt/nas/";
 	var urlNAS = "../mnt/nas/";
 	var findURL = req.params.name;
 	var newURL = urlNAS+findURL;
